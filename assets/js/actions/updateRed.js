5b5a9516e0fa90597add49ddd879855f2757f971
Niels Simenon <niels.simenon@gmail.com>
2016-12-10 22:42:50 +0000

    Resetting channels from Calculated to defaults on input.

--- assets/js/actions/updateRed.js
+++ assets/js/actions/updateRed.js
@@ -3,41 +3,59 @@
 const rgbToHsv = require('../rgbToHsv.js');
 
 module.exports = function(red, socket) {
-  return function(dispatch, getState) {
-    const color = getState().get('color');
-    const rgb = color.get('rgb').toObject();
-    const hsv = rgbToHsv(red, rgb.green, rgb.blue).hsv;
-    const sync = color.get('sync');
+    return function(dispatch, getState) {
+        let color = getState().get('color');
+        let rgb = color.get('rgb').toObject();
+        let hsv = rgbToHsv(red, rgb.green, rgb.blue).hsv;
+        const sync = color.get('sync');
 
-    dispatch({
-      type: 'UPDATE_RED',
-      data: red
-    });
+        dispatch({
+            type: 'UPDATE_RED',
+            data: red
+        });
 
-    if(socket) {
-      socket.send(JSON.stringify(['mode', 'rgb']));
-      socket.send(JSON.stringify(['rgb', {...rgb, red}]));
-    }
+        if(Number.isNaN(rgb.green)) {
+            dispatch({
+                type: 'UPDATE_GREEN',
+                data: 255
+            });
+        }
 
-    if(sync) {
-      dispatch({
-        type: 'UPDATE_HUE',
-        data: hsv.hue
-      });
+        if(Number.isNaN(rgb.blue)) {
+            dispatch({
+                type: 'UPDATE_BLUE',
+                data: 255
+            });
+        }
 
-      dispatch({
-        type: 'UPDATE_SATURATION',
-        data: hsv.saturation
-      });
+        color = getState().get('color');
+        rgb = color.get('rgb').toObject();
+        hsv = rgbToHsv(rgb.red, rgb.green, rgb.blue).hsv;
 
-      dispatch({
-        type: 'UPDATE_VALUE',
-        data: hsv.value
-      });
+        if(socket) {
+            socket.send(JSON.stringify(['mode', 'rgb']));
+            socket.send(JSON.stringify(['rgb', rgb]));
+        }
 
-      if(socket) {
-        socket.send(JSON.stringify(['hsv', hsv]));
-      }
-    }
-  };
+        if(sync) {
+            dispatch({
+                type: 'UPDATE_HUE',
+                data: hsv.hue
+            });
+
+            dispatch({
+                type: 'UPDATE_SATURATION',
+                data: hsv.saturation
+            });
+
+            dispatch({
+                type: 'UPDATE_VALUE',
+                data: hsv.value
+            });
+
+            if(socket) {
+                socket.send(JSON.stringify(['hsv', hsv]));
+            }
+        }
+    };
 };
