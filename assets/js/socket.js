85a3bf2dd03368974e9bd4b8deecf5e48e1fb5c5
Niels Simenon <niels.simenon@gmail.com>
2016-12-30 11:48:39 +0000

    Calculating other color channel on load.

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -1,7 +1,42 @@
 'use strict';
 
 import config from 'config';
+import convert from 'color-convert';
+
+import store from './store';
 
 const socket = new WebSocket(config.socket.host);
+
+socket.addEventListener('message', msg => {
+    const data = JSON.parse(msg.data);
+
+    if(data[0] === 'rgb') {
+        store.commit('setRed', data[1].red);
+        store.commit('setGreen', data[1].green);
+        store.commit('setBlue', data[1].blue);
+
+        const hsv = convert.rgb.hsv.raw(Number(data[1].red), Number(data[1].green), Number(data[1].blue));
+
+        if(store.state.pattern.sync) {
+            store.commit('setHue', hsv[0]);
+            store.commit('setSaturation', hsv[1] / 100);
+            store.commit('setValue', hsv[2] / 100);
+        }
+    }
+
+    if(data[0] === 'hsv') {
+        store.commit('setHue', data[1].hue);
+        store.commit('setSaturation', data[1].saturation);
+        store.commit('setValue', data[1].value);
+
+        const rgb = convert.hsv.rgb.raw(Number(data[1].hue), Number(data[1].saturation) * 100, Number(data[1].value) * 100) || [NaN, NaN, NaN];
+
+        if(store.state.pattern.sync) {
+            store.commit('setRed', rgb[0]);
+            store.commit('setGreen', rgb[1]);
+            store.commit('setBlue', rgb[2]);
+        }
+    }
+});
 
 export default socket;
--- src/modifiers/color/init.js
+++ src/modifiers/color/init.js
@@ -6,82 +6,85 @@ const util = require('util');
 const math = require('mathjs');
 
 const init = function(leds, ws) {
-  const rgb = {
-    red: {
-        value: 255,
-        eval: () => 255
-    },
-    green: {
-        value: 0,
-        eval: () => 0
-    },
-    blue: {
-        value: 0,
-        eval: () => 0
-    }
-  };
-
-  const hsv = {
-    hue: {
-        value: 0,
-        eval: () => 0
-    },
-    saturation: {
-        value: 1,
-        eval: () => 1
-    },
-    value: {
-        value: 1,
-        eval: () => 1
-    }
-  };
-
-  ws.on('connection', wss => {
-    wss.transfer('rgb', {red: rgb.red.value, green: rgb.green.value, blue: rgb.blue.value});
-    wss.transfer('hsv', {hue: hsv.hue.value, saturation: hsv.saturation.value, value: hsv.value.value});
-
-    wss.on('message', msg => {
-      const data = JSON.parse(msg);
-
-      if(data[0] === 'rgb') {
-        try {
-          Object.keys(data[1]).forEach(prop => {
-            rgb[prop] = {
-              value: data[1][prop],
-              eval: math.compile(data[1][prop]).eval
-            };
-          });
-
-          wss.broadcast('rgb', data[1]);
-        } catch(error) {
-          wss.transfer('error', error.message);
+    const rgb = {
+        red: {
+            value: 255,
+            eval: () => 255
+        },
+        green: {
+            value: 0,
+            eval: () => 0
+        },
+        blue: {
+            value: 0,
+            eval: () => 0
         }
-      }
+    };
 
-      if(data[0] === 'hsv') {
-        try {
-          Object.keys(data[1]).forEach(prop => {
-            hsv[prop] = {
-              value: data[1][prop],
-              eval: math.compile(data[1][prop]).eval
-            };
-          });
-
-          wss.broadcast('hsv', data[1]);
-        } catch(error) {
-          wss.transfer('error', error.message);
+    const hsv = {
+        hue: {
+            value: 0,
+            eval: () => 0
+        },
+        saturation: {
+            value: 1,
+            eval: () => 1
+        },
+        value: {
+            value: 1,
+            eval: () => 1
         }
-      }
+    };
+
+    ws.on('connection', wss => {
+        if(leds.mode === 'rgb') {
+            wss.transfer('rgb', {red: rgb.red.value, green: rgb.green.value, blue: rgb.blue.value});
+        } else if(leds.mode === 'hsv') {
+            wss.transfer('hsv', {hue: hsv.hue.value, saturation: hsv.saturation.value, value: hsv.value.value});
+        }
+
+        wss.on('message', msg => {
+            const data = JSON.parse(msg);
+
+            if(data[0] === 'rgb') {
+                try {
+                    Object.keys(data[1]).forEach(prop => {
+                        rgb[prop] = {
+                            value: data[1][prop],
+                            eval: math.compile(data[1][prop]).eval
+                        };
+                    });
+
+                    wss.broadcast('rgb', data[1]);
+                } catch(error) {
+                    wss.transfer('error', error.message);
+                }
+            }
+
+            if(data[0] === 'hsv') {
+                try {
+                    Object.keys(data[1]).forEach(prop => {
+                        hsv[prop] = {
+                            value: data[1][prop],
+                            eval: math.compile(data[1][prop]).eval
+                        };
+                    });
+
+                    wss.broadcast('hsv', data[1]);
+                } catch(error) {
+                    wss.transfer('error', error.message);
+                }
+            }
+        });
     });
-  });
 
-  const scope = {};
+    const scope = {};
 
-  scope.length = scope.l = leds.pixels.length;
-  scope.width = scope.w = config.size[0] || config.size;
-  scope.height = scope.h = config.size[1] || 1;
+    scope.length = scope.l = leds.pixels.length;
+    scope.width = scope.w = config.size[0] || config.size;
+    scope.height = scope.h = config.size[1] || 1;
 
-  return {rgb, hsv, scope};
+    return {rgb, hsv, scope};
 };
 
 module.exports = init;
